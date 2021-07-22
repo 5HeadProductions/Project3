c7e89a9e10d048f6b392b00f6ebc472de4675c52
Andres Maturin <idontknowwhattonamethiskhay@gmail.com>
2021-07-22 14:08:24 -0700

    Buttons are all set for going to and from the play button

--- Assets/Scripts/CanvasSwitch.cs
+++ Assets/Scripts/CanvasSwitch.cs
@@ -8,35 +8,53 @@ public class CanvasSwitch : MonoBehaviour
     [SerializeField]private GameObject mmCanvas, playCanvas;
     [SerializeField]private CanvasGroup mmCanvasGroup, playCanvasGroup;
     [SerializeField]private Animator playAnimator,mmAnimator;
-
+    public float transitionTime;
+    private bool isFaded = false;
 //player clicks on the play button
     public void SwitchToPlay(){ 
-       playAnimator.SetTrigger("Start");
-       mmAnimator.SetTrigger("Start");
-     StartCoroutine(Fade());
-       
+      // playAnimator.SetTrigger("Start");
+      // mmAnimator.SetTrigger("Start");
+
+      //FadeOut takes in the stating value, and an ending value
+      // if faded is false then we want to go from 1(current alpha value) -> 0 (determined by the condition)
+      // if faded is true we want to go from 0(current alpha value) -> 1(determined by the condition)
+     StartCoroutine(FadeOut(mmCanvasGroup.alpha, isFaded ? 1 : 0));
+     isFaded = !isFaded;  // updating if the canvas has faded or not
     }
 
     //players clicks on the back button from Play Canvas
     public void SwitchToMM(){
-        StopAllCoroutines();
-      StartCoroutine(FadeIn());
-        playAnimator.SetBool("Done", true);
+      StartCoroutine(FadeOut(mmCanvasGroup.alpha, isFaded ? 1 : 0));
+      isFaded = !isFaded;
+      //  playAnimator.SetBool("Done", true);
       
  
     }
 
-    IEnumerator Fade(){
-         yield return new WaitForSeconds(1.5f);
-          mmCanvas.SetActive(false);
-       mmCanvasGroup.interactable = false;
+    IEnumerator FadeOut(float start, float end){
+
+          if(end == 0)playCanvas.SetActive(true); //back
+          if(end == 1)mmCanvas.SetActive(true);//play
+          float counter = 0f;
+          while(counter < transitionTime){
+              counter += Time.deltaTime;
+              mmCanvasGroup.alpha = Mathf.Lerp(start, end, counter / transitionTime);
+              yield return null;
+
+          }
+         //  if(end == 0)playCanvas.GetComponent<Canvas>().sortingOrder = 1; //back
+         //  if(end == 0)mmCanvas.GetComponent<Canvas>().sortingOrder = 0;//back
+           if(end == 0)mmCanvas.SetActive(false);//back
+
+         //  if(end == 1)mmCanvas.GetComponent<Canvas>().sortingOrder = 1;//play
+         //  if(end == 1)playCanvas.GetComponent<Canvas>().sortingOrder = 0;//play
+           if(end == 1)playCanvas.SetActive(false);//play
+
     }
 
-     IEnumerator FadeIn(){
-          mmCanvas.SetActive(true);
-         yield return new WaitForSeconds(1.5f);
-          mmAnimator.SetBool("Done", true);
-        mmCanvasGroup.interactable = false;
-    }
+    // IEnumerator FadeIn(){
+    //      mmCanvas.SetActive(true);
+
+   // }
 
 }
