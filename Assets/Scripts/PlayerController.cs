aa339825e05d16308dc83383490b5a4f1b5f57e7
EdenArts <ojedaeden08@gmail.com>
2021-07-26 13:07:17 -0700

    commenting code and attempting to fix charge display

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,13 +9,19 @@ public class PlayerController : MonoBehaviour
 
         PhotonView view;
     	public float moveSpeed;
-        
+        [SerializeField]Transform firePoint;
+        [SerializeField] float projectileSpeed;
 
+        private float _timeUntilFire = 0;
+
+        //Shooting Cooldown
+        [SerializeField] float timeBetweenProjectiles;
+        
         public Transform rocketSprite;
 
         private float _chargeTimer = 0;
 
-        
+        //////////////////////Projectile Info/////////////////////////////////////////////////////
         public Projectile tier1Bullet;
         public float bullet2Timer;
         public Projectile tier2Bullet;
@@ -28,20 +34,28 @@ public class PlayerController : MonoBehaviour
         public GameObject charge1;
         public GameObject charge2;
         public GameObject charge3;
-
+        ////////////////////////////////////////////////////////////////////////////////////////
         void Start(){
+            //View component for the photon network
             view = gameObject.GetComponent<PhotonView>();
+            //Sets the bullet prefab that will be instantiated if fire is called
             SetBullet(tier1Bullet.bulletPrefab);
+            
+            //sets all charges to false so they cannot be seen until called upon
             charge1.SetActive(false);
             charge2.SetActive(false);
             charge3.SetActive(false);
+            
         }
 
 
     void Update(){
+        //the if statement is so you only control one player
         if(view.IsMine){
         RotatePlayer();
 
+        //this is a check so fire cannot be called immediatly
+        if(_timeUntilFire <= Time.time)
         WeaponCharge();
         }
 
@@ -55,30 +69,39 @@ public class PlayerController : MonoBehaviour
 		rocketSprite.localEulerAngles = new Vector3(0, 0, Input.GetAxis("Horizontal") * -30);
 	}
 
-//Instantiates a bullet prefab
+    //Instantiates a bullet prefab
     void Fire(){
-      GameObject bullet = PhotonNetwork.Instantiate(_currentBullet.name,rocketSprite.position,transform.rotation);
+        //so the bullet can be seen by other player
+        GameObject bullet = PhotonNetwork.Instantiate(_currentBullet.name,rocketSprite.position,transform.rotation);
+        //giving the bullet velocity
+        bullet.GetComponent<Rigidbody2D>().velocity = rocketSprite.position.normalized * projectileSpeed;
 
-      bullet.GetComponent<Rigidbody2D>().velocity = rocketSprite.position.normalized * 100;
+        //sets bullet to the first one in case a charged bullet is set
+        SetBullet(tier1Bullet.bulletPrefab);
 
-    SetBullet(tier1Bullet.bulletPrefab);
-    charge1.SetActive(false);
-    charge2.SetActive(false);
-    charge3.SetActive(false);
+    
+    //sets all charges to false so all charges are off when a bullet is instantiated
+        charge1.SetActive(false);
+        charge2.SetActive(false);
+        charge3.SetActive(false);
+        _timeUntilFire = Time.time + timeBetweenProjectiles;
     }
 
     
-
+    //helper funtion for the WeaponCharge method
     void SetBullet(GameObject curretBullet){
         _currentBullet = curretBullet;
     }
 
+    //called in update weapon charge is increasing a timer while the space key is held down, the timer is what determines what is current
+    //bullet that will be called by the fire function
     void WeaponCharge(){
+        
         if(Input.GetKey(KeyCode.Space)){
             _chargeTimer += Time.deltaTime;
             DisplayCharge();
         }
-        //Tier 1
+        
         if(Input.GetKeyUp(KeyCode.Space)){
         if(_chargeTimer > bullet2Timer)
             SetBullet(tier2Bullet.bulletPrefab);
@@ -92,9 +115,13 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    //called by the weapon charge function, determines what charge is being displayed by also checking the timer
     void DisplayCharge(){
-        if(_chargeTimer > bullet2Timer)
+        
+        
+        if((_chargeTimer > bullet2Timer)){
             charge1.SetActive(true);
+        }
         if(_chargeTimer > bullet3Timer){
             charge1.SetActive(false);
             charge2.SetActive(true);
